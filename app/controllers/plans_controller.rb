568225ae02f5916a8099fb0d47867abb99215591
Naoki-Kakimura <kn1210zzr@outlook.jp>
2021-02-17 20:01:01 +0900

    ランダムな市町村区取得実装(環境変数化済)

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,7 +3,22 @@ class PlansController < ApplicationController
 
   end
   def new
-    
+    require 'securerandom'
+    prefecture_num = SecureRandom.random_number(47)
+    params =URI.encode_www_form({prefCode:prefecture_num})
+    uri = URI.parse("https://opendata.resas-portal.go.jp/api/v1/cities?#{params}")
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req["X-API-KEY"] = ENV['RESAS_KEY']
+
+    https = Net::HTTP.new(uri.host, uri.port)
+    https.use_ssl = true
+    res = https.request(req)
+
+    res_body = JSON.parse(res.body)
+    result = res_body["result"]
+    city_num = SecureRandom.random_number(result.length)
+    city_hash = result[city_num]
+    @city_name = city_hash["cityName"]
   end
   def create
     plan = Plans.new
