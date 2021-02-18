0e9cd51446ef9f5962823dfaea3dbfd90eca4365
Naoki-Kakimura <kn1210zzr@outlook.jp>
2021-02-18 12:20:51 +0900

    Flickr実装完了

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -4,6 +4,18 @@ class PlansController < ApplicationController
   end
   def new
     require 'securerandom'
+    require 'flickraw'
+    search_trip
+    if @image_url == []
+      search_trip
+    end
+    @plan = Plan.new
+  end
+  def create
+    Plan.create(plan_params)
+  end
+  private
+  def search_trip
     prefecture_num = SecureRandom.random_number(47)
     params =URI.encode_www_form({prefCode:prefecture_num})
     uri = URI.parse("https://opendata.resas-portal.go.jp/api/v1/cities?#{params}")
@@ -19,8 +31,17 @@ class PlansController < ApplicationController
     city_num = SecureRandom.random_number(result.length)
     city_hash = result[city_num]
     @city_name = city_hash["cityName"]
+
+    FlickRaw.api_key = ENV["FLICKR_KEY"]
+    FlickRaw.shared_secret = ENV["FLICKR_SECRET_KEY"]
+    images = flickr.photos.search(tags: @city_name, sort: "interestingness-desc", per_page: 4)
+    @image_url = []
+    images.each do |image|
+      url = FlickRaw.url image
+      @image_url.push(url)
+    end
   end
-  def create
-    plan = Plans.new
+  def plan_params
+    params.require(:plan).permit(:name)
   end
 end
