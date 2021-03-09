e41d5c62cb84982b3cb666ae5e354341c0e841c4
Naoki-Kakimura <kn1210zzr@outlook.jp>
2021-03-09 19:10:54 +0900

    ストリートビュー画像の表示機能実装

--- app/models/visit_point.rb
+++ app/models/visit_point.rb
@@ -1,3 +1,4 @@
 class VisitPoint < ApplicationRecord
   belongs_to :visit_place
+  has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "plans#index"
   resources :plans, only:[:index,:new,:create]
-  resources :maps, only:[:new]
+  resources :maps, only:[:new,:create]
+  resources :streets, only:[:new,:create]
 end
